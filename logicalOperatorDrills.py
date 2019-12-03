3c1813fa31e73addff4725921495d35cec22b9e5
lezlyv <57326531+lezlyv@users.noreply.github.com>
2019-12-02 20:17:58 -0600

    Update logicalOperatorDrills.py

--- logicalOperatorDrills.py
+++ logicalOperatorDrills.py
@@ -69,7 +69,7 @@ print(j)
 #10) Check if NOT(1 is greater than 2) and store the answer in variable k
 k = not (1 > 2) 
 print(k) 
-
+#True 
 '''
 For the next section, you will be reading comparison statements and determining their answer
 EXAMPLE:
@@ -86,75 +86,76 @@ START HERE
 '''
 
 #1) 4 == 5 and 5 == 5
-
+False 
 #2) 5 == 5 or 5 == 4
-
+True 
 #3) not 3 == 3
-
+False 
 #4) a = 2
 # ) not a == 2
-
+False 
 #5) a = 3
 # ) b = 2
 # ) a == b and b == 4
-
+False 
 #6) a = 4
 # ) b = 4
 # ) a == b or b == 4
-
+True 
 #7) a = 6
 # ) b = (12/2)
 # ) a == b and b == (24/4)
-
+True 
 #8) a = (3/3)
 # ) b = (13/13)
 # ) a == b or b == 1
-
+True 
 #9) a = 3
 # ) b = math.sqrt(9)
 # ) c = 2
 # ) a == b and b == c
-
+False 
 #10) a = "word"
 #  ) b = "Word"
 #  ) c = "Word"
 #  ) a == b or b == c
-
+True 
 #11) (10/5) == (12/6) and math.sqrt(9) == 3
-
+True 
 #12) (14/7) == (15/5) or math.sqrt(9) == 3
-
+True 
 #13) a = 10
 #  ) b = 5
 #  ) (a/b) == 2 and 2 == 1
-
+False 
 #14) a = 10
 #  ) b = 5
 #  ) (a/b) == (12/6)
-
+True 
 #15) a = 10
 #  ) b = 5
 #  ) c = 12
 #  ) d = 6
 #  ) (a/b) == (12/6) and (c/2) == d
-
+True 
 #16) a = "something"
 #  ) b = "some thing"
 #  ) a == b and b == 2
-
+False 
 #17) a = "word"
 #  ) b = "word"
 #  ) word = "incorrect"
 #  ) a == b and b == word
-
+False 
 #18) a = True
 #  ) a == True and not True == False
-
+True  
 #19) a = True
 #  ) b = False
 #  ) a == b or not a == b
-
+True 
 #20) a = True
 #  ) A = False
 #  ) b = False
 #  ) A == b or a == b
+True 
